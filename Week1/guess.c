81188e2bf65a51d69734fd5b937c0488b8ef3238
Alex Mononen <almo121@live.com>
2017-06-22 13:04:07 -0700

    week 1 completed files

---
+++ Week1/guess.c
@@ -0,0 +1,73 @@
+/* All code originally written by Alex Mononen */
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+	//printing out instructions and confirming player readiness
+	printf("Welcome to the game of Guess It! \n");
+	printf("I will choose a number between 1 and 200 \n");
+	printf("You will try to guess that number. If you guess wrong, I will tell you if you guessed too high to too low.\n");
+	printf("You will have 5 tires to get the number.\n");
+	printf("OK. I am thinking of a number. Try to guess it.\n");
+	//declaration of the function
+	int guessfunc = guessing();
+}
+
+int guessing(void)
+{
+	//creating bool datatype
+	typedef int bool;
+#define true 1;
+#define false 0;
+	//requsition of different seed at start of program, ensures different random numbers.
+	srand(time(NULL));
+	//random number generation from 1 - 200
+	int random = rand() % 200 + 1;
+
+	//guessing
+	bool win = false;
+	int i = 0;
+	while (i < 5)
+	{
+		i++;
+		{
+			printf("Your guess?\n");
+			int guess;
+			scanf_s("%d", &guess);
+			//logic!
+			if (guess > random && guess < 200)
+			{
+				printf("Too high!\n");
+			}
+			else if (guess < random && guess > 0)
+			{
+				printf("Too low!\n");
+			}
+			else if (guess == random)
+			{
+				printf("*****CORRECT*****");
+				printf("Want to play again?");
+				char again;
+				scanf_s(" %c", &again);
+				if (again == 'y')
+				{
+					//redeclaration of the function
+					int guessfunc = guessing();
+				}
+				else
+				{
+					printf("Goodbye, It was fun. Play again soon.");
+					break;
+					break;
+				}
+				break;
+			}
+			else
+			{
+				printf("Illegeal guess. Your number must be between 1 and 200.");
+			}
+		}
+	}
+	return 0;
+}
---
+++ Week1/math.c
@@ -0,0 +1,27 @@
+/* All code originally written by Alex Mononen */
+#include <stdio.h>
+
+int main(void) 
+{
+	printf("Enter an integer: ");
+	int int1;
+	scanf_s(" %d", &int1);
+	printf("\n Enter another integer: ");
+	int int2;
+	scanf_s(" %d", &int2);
+	int add = int1 + int2;
+	int sub = int1 - int2;
+	printf("%d", int1);
+	printf(" + ");
+	printf("%d", int2);
+	printf(" = ");
+	printf("%d", add);
+	printf("\n");
+	printf("%d", int1);
+	printf(" - ");
+	printf("%d", int2);
+	printf(" = ");
+	printf("%d", sub);
+	getchar();
+	getchar();
+}
