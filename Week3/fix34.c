ed7962c9e267e96bb44a9bbfec11f25c8b89fcc8
Alex <almo121@live.com>
2017-07-04 17:51:42 -0700

    IT WORKS! (need to find a way to loop lines 63-71

--- Week3/fix34.c
+++ Week3/fix34.c
@@ -6,20 +6,24 @@
 #include <stdio.h>;
 #include <stdbool.h>;
 #include <math.h>;
-int fix34(int input[]);
-void printArray(int original[], int fixed[]);
+/*Structure of 9 int to use to return the damn function thingy*/
+typedef struct
+{
+	int a, b, c, d, e, f, g, h, i;
+}Values;
+//int fix34(int input[]);
+void printArray(int input[]);
 int main(void)
 {
 	int array[9] = { 1, 3, 8, 5, 4, 3, 6, 4, 5 };
-	//int sorted[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	int sorted = fix34(array);
-	printArray(array, sorted);
+	printArray(array);
 	getchar();
+	
 }
 
-int fix34(int input[])
+Values fix34(int input[])
 {
-	//int tmp[9] = array;
+	Values result;	/*Declare 'result' of type 'Values'*/
 	int input3[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	int non3[3] = { 0, 0, 0 };
 	int i = 0;
@@ -56,25 +60,31 @@ int fix34(int input[])
 			n++;
 		}
 	}
-	static int result[9];
-	for (int j = 0; j <= 9; j++)
-	{
-		result[j] = input3[j];
-	}
+	result.a = input3[0];
+	result.b = input3[1];
+	result.c = input3[2];
+	result.d = input3[3];
+	result.e = input3[4];
+	result.f = input3[5];
+	result.g = input3[6];
+	result.h = input3[7];
+	result.i = input3[8];
 	return result;
 }
 
-void printArray(int original[], int fixed[])
+void printArray(int input[])
 {
+	Values result = fix34(input);
+	int dump[9] = { result.a, result.b, result.c, result.d, result.e, result.f, result.g, result.h, result.i };
 	printf("fix34({");
 	for (int i = 0; i <= 8; i++)
 	{
-		printf("%d, ", original[i]);
+		printf("%d, ", input[i]);
 	}
 	printf("}) => {");
 	for (int i = 0; i <= 8; i++)
 	{
-		printf("%d, ", fixed[i]);
+		printf("%d, ", dump[i]);
 	}
 	printf("}");
 }
